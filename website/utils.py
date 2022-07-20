78093279c95be53e47cc695eab558c743212b877
ecedreamer <sangit.niroula@gmail.com>
2022-07-20 21:59:18 +0545

    sse test

--- website/utils.py
+++ website/utils.py
@@ -1,11 +1,19 @@
 from django.http import StreamingHttpResponse
 import datetime
 import time
+from quizapp.models import Question
+
+
+def event_stream():
+    old_count = Question.objects.count()
+    while True:
+        time.sleep(2)
+        new_count = Question.objects.count()
+        if old_count != new_count:
+            old_count = new_count
+            yield f'data: Number of question is {new_count} \n\n'
 
 
 def server_sent_event(request):
-    def event_stream():
-        while True:
-            time.sleep(3)
-            yield 'data: The server time is: %s\n\n' % datetime.datetime.now()
+    
     return StreamingHttpResponse(event_stream(), content_type='text/event-stream')
